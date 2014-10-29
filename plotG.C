4a5c7d3244f5600ffe9a56f06601e2e3c9d3c6a6
gsize <gsizeg@gmail.com>
2014-10-29 15:41:22 +0800

    fix calculate error

--- plotG.C
+++ plotG.C
@@ -1,8 +1,7 @@
 
-
 Double_t fun(Double_t *x, Double_t *par) {
    
-   Double_t result = x[0]*x[0]/(4.* x[1]*x[1]);
+   Double_t result = 0.5*(1. - x[1]/TMath::Sqrt(x[1]*x[1]+x[0]*x[0]));
    return result;
 }
 
@@ -14,10 +13,11 @@ gStyle->SetPalette(1);
 
 Double_t x_min = 0.5;
 Double_t x_max = 6.;
-Double_t y_min = 30.;
-Double_t y_max = 300.;
+Double_t y_min = 10.;
+Double_t y_max = 200.;
 
 TF2 *f2 = new TF2("f2",fun,x_min,x_max,y_min,y_max,0);
+
 f2->GetXaxis()->SetTitle("r/cm");
 f2->GetXaxis()->SetTitleOffset(1.5);
 f2->GetYaxis()->SetTitle("L/cm");
@@ -33,7 +33,7 @@ gPad->SetLogz(1);
 f2->Draw("surf1");
 
 TPaveLabel *title = new TPaveLabel(0.74, 0.86, 0.96, 0.98,
-                           "P=#frac{r^{2}}{4L^{2}}");
+                           "P=0.5*(1-h#/#sqrt{L^{2}+r^{2}})");
 //   title->SetFillColor(32);
    title->Draw();
 
