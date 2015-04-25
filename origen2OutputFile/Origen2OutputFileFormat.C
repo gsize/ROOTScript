016a2243cded1104621eb6316ab440e5fd8cab49
gsize <gsizeg@gmail.com>
2015-04-25 18:17:31 +0800

    使用正则表达式实现文本匹配

--- origen2OutputFile/Origen2OutputFileFormat.C
+++ origen2OutputFile/Origen2OutputFileFormat.C
@@ -1,92 +1,63 @@
 // origen2的输出文件格式化
 
-void Origen2OutputFileFormat()
+void Origen2OutputFileFormat(TString fname="TAPE20d.OUT")
 {
 	TString dir = gSystem->UnixPathName(gInterpreter->GetCurrentMacroName());
 	dir.ReplaceAll("Origen2OutputFileFormat.C","");
 	dir.ReplaceAll("/./","/");
 	TString pathName;
-	pathName = dir + "data/TAPE20d.OUT";
-	cout << pathName <<endl;
+	pathName = dir + "data/";
+	pathName+=fname;
 
-    ifstream in;
-    in.open(pathName.Data());
+	ifstream in;
+	in.open(pathName.Data());
 	if(!(in.is_open()))
 	{
 		cout<< "open in file failed!"<<endl;
 		return ;
 	}
-    int nlines=0;
-    int nlist=0;
-    int list_flg=0;
-    int iflg=0;
-    int page=0;
-    int flg_r=0;
 
-    string str_tmp;
-    vector<string> vec_str;
+	string str_tmp;
+	vector<string> vec_str;
+	TPMERegexp re_start("^\\+ +FISSION PRODUCTS +");
+	TPMERegexp re_start1("^0 +[0-9a-zA-z :,]+RADIOACTIVITY...CURIES +");
+	TPMERegexp re_end("^1 +OUTPUT");
+	TPMERegexp re4("^ +TOTAL ");
+	TPMERegexp re(".{12,12}( [0-9]+(\\.[0-9]+)(E|e)(\\+|\\-)[0-9]{2,2}){10,10}");
 
-    while(getline(in,str_tmp)) {
-        //if(!in.good()) break;
-        //getline(in,str_tmp);
-        nlines++;
-        if(iflg) {
-            nlist++;
-            list_flg++;
-            //flg_r=1;
-            if(list_flg<5) {
-                if(str_tmp.find("CURIES")!=string::npos) {
-                    flg_r=1;
-                    //printf("%d\n",list_flg);
-                    page++;
-                }
-                if(page==1)
-                if(list_flg==4)
-                    vec_str.push_back(str_tmp);
-                if(list_flg==2){
-                    if(!flg_r)
-                    {
-                    list_flg=0;
-                    iflg=0;
-                    flg_r=0;
-                    }
-                }
-                continue;
-            }
-            if(flg_r) {
-                if(list_flg>54 || str_tmp.find("TOTAL")!=string::npos) {
-                        if(str_tmp.find("TOTAL")!=string::npos)
-                        {
-                            vec_str.push_back(str_tmp);
-                            break;
-                        }
-                    list_flg=0;
-                    iflg=0;
-                    flg_r=0;
-                    continue;
-                }
-                vec_str.push_back(str_tmp);
-            }
-        }
-        if(str_tmp.find("+")!=string::npos && str_tmp.find("FISSION PRODUCTS")!=string::npos) {
-            iflg=1;
-            //printf("%d\n",nlines);
-        }
-
-    }
-    printf("ok\n page=%d\nnlines=%d\n",page,nlines);
-
+	while(getline(in,str_tmp)) 
+	{
+		TString str(str_tmp);
+		if(str.Contains((TPRegexp) re_start))
+		{
+			getline(in,str_tmp);
+			getline(in,str_tmp);
+			str=str_tmp;
+			if(str.Contains((TPRegexp) re_start1))
+			{
+				getline(in,str_tmp);
+				getline(in,str_tmp);
+				while(getline(in,str_tmp)) {
+					str=str_tmp;
+					if(str.Contains((TPRegexp) re_end))
+						break;
+					vec_str.push_back(str_tmp);
+				}
+			}
+		}
+	}
+	in.close();
 	TString outFile;
-	outFile = dir + "outdata4.txt";
-    ofstream out;
-    out.open(outFile.Data());
+	outFile = dir + fname;
+	outFile += ".txt";
+	ofstream out;
+	out.open(outFile.Data());
 	if(!(out.is_open()))
 	{
 		cout<< "open in file failed!"<<endl;
 		return ;
 	}
-    for(int ii=0; ii<vec_str.size(); ii++)
-        out<<vec_str[ii]<<endl;
-    out.close();
-    in.close();
+	for(int ii=0; ii<vec_str.size(); ii++)
+		out<<vec_str[ii]<<endl;
+	out.close();
 }
